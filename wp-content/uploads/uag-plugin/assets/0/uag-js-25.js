c76451eb4de836bc1bc3f43b837388ab540dacb5
WillianMiranda7 <willianmiranda243@gmail.com>
2024-02-22 00:40:55 -0300

    22/02/2024 valores simbólicos

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-25.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
+	UAGBTableOfContents._run( {"mappingHeaders":[false,false,true,false,false,false],"scrollToTop":false,"makeCollapsible":false}, '.uagb-block-399d43c6' );
+} );
+ });
